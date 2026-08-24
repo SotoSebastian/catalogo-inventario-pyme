441d07e9319efed77874a3f9c18bd2c5d80fff31
SotoSebastian <sebalxt3@gmail.com>
2026-08-24 19:42:11 -0400

    Se crean migraciones para las tablas de = categories;products;product_movement;users.

---
+++ database/migrations/2026_08_24_222723_create_product_movement_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_movement', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->restrictOnDelete();
+            $table->foreignId('product_id')->constrained()->restrictOnDelete();
+            $table->string('type');
+            $table->integer('mount');
+            $table->string('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_movement');
+    }
+};
